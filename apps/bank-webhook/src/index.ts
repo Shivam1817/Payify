6647d949321677b921b23f5cb6cf60dcb6a0a86d
Shivam <shivam.chem21@iitg.ac.in>
2025-09-11 16:36:02 +0530

    backend hdfc- webhook

--- apps/bank-webhook/src/index.ts
+++ apps/bank-webhook/src/index.ts
@@ -1,12 +1,49 @@
 import express from 'express';
-
+import db from '@repo/db/client';
 const app = express();
 
-app.post('/hdfcWebhook', (req, res) => {
-    const paymentinformation = {
+app.use(express.json());
+
+app.post('/hdfcWebhook', async(req, res) => {
+    const paymentinformation:{
+        token: string,
+        userId: string,
+        amount: number
+    } = {
         token: req.body.token,
         userId: req.body.userId,
         amount: req.body.amount,
+    };
+    try {
+        await db.$transaction([
+            db.balance.updateMany({
+                where: {
+                    userId: Number(paymentinformation.userId)
+                },
+                data: {
+                    amount: {
+                        increment: paymentinformation.amount
+                    }
+                }
+            }),
+            db.onRampTransaction.updateMany({
+                where: {
+                    userId: Number(paymentinformation.userId),
+                },
+                data: {
+                    status: "Success",
+                }
+            })
+        ]);
+        res.json({
+            message: "Captured"
+        })
+    }
+    catch(e){
+        console.error(e);
+        res.status(411).json({
+            message: "Error while processing webhook"
+        })
     }
 })
 
